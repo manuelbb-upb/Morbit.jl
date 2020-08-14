a70d2ec205675788c41bb6fb4b17d195e5992d37
Manuel Berkemeier <manuelbb@math.upb.de>
2020-08-14 18:44:47 +0200

    fix in compile script

--- examples/compile_sysimage.jl
+++ examples/compile_sysimage.jl
@@ -13,6 +13,6 @@ Pkg.activate(joinpath( @__DIR__, ".." ) )
 create_sysimage(
 	[:Morbit];
 	sysimage_path = OUTFILENAME,
-	precompile_execution_file = "precompile_morbit.jl"
+	precompile_execution_file = joinpath(@__DIR__,"precompile_morbit.jl")
 	)
 
