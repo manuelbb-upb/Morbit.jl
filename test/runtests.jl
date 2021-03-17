4c26f31c6966afa505114e3ffd6c4147d2696574
Manuel Berkemeier <manuelbb@mail.uni-paderborn.de>
2021-03-17 15:46:48 +0100

    mainly deps fixes

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
 using Morbit
 using Test
 
-test_files = ["add_objectives.jl", "lagrange_model.jl", "optimization.jl", "x_stop_function.jl"]
+# test_files = ["add_objectives.jl", "lagrange_model.jl", "optimization.jl", "x_stop_function.jl"]
 # "rbf_derivatives.jl", "batch_objectives.jl",, "saving.jl"]
 
+test_files = ["optimization.jl",]
 for fn in test_files
     include( joinpath(@__DIR__, fn) )   # prefixe with path for PackageCompiler
 end
