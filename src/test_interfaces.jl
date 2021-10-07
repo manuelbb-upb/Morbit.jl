24260319c285f991b35617d0870aa0514e5f9f82
Manuel Berkemeier <manuelbb@mail.uni-paderborn.de>
2021-10-07 18:40:16 +0200

    backup before weekend

--- src/test_interfaces.jl
+++ src/test_interfaces.jl
@@ -6,14 +6,7 @@ M.print_all_logs()
 mop = M.MOP(3)
 
 vars = M.var_indices(mop)
-#=
-M.add_lower_bound!( mop, vars[1], -10 )
-M.add_lower_bound!( mop, vars[2], -3 )
-M.add_upper_bound!( mop, vars[1], 10 )
-M.add_upper_bound!( mop, vars[2], 10)
-M.add_lower_bound!( mop, vars[3], -10)
-M.add_upper_bound!( mop, vars[3], 10)
-=#
+
 f1 = x -> sum((x.-1).^2)
 f2 = x -> sum((x.+1).^2)
 F = x -> [f1(x);f2(x)]
@@ -24,6 +17,16 @@ oind = M.add_objective!(mop, f1; model_cfg = M.RbfConfig() )
 #oind2 = M.add_objective!(mop, x -> sum((x.+1).^2); model_cfg = M.TaylorConfig() )
 #oind2 = M.add_objective!(mop, x -> sum((x.+1).^2); model_cfg = M.TaylorCallbackConfig(), diff_method = M.AutoDiffWrapper )
 oind2 = M.add_objective!(mop, f2; model_cfg = M.ExactConfig(), diff_method = M.AutoDiffWrapper )
+
+
+#=
+M.add_lower_bound!( mop, vars[1], -10 )
+M.add_lower_bound!( mop, vars[2], -3 )
+M.add_upper_bound!( mop, vars[1], 10 )
+M.add_upper_bound!( mop, vars[2], 10)
+M.add_lower_bound!( mop, vars[3], -10)
+M.add_upper_bound!( mop, vars[3], 10)
+=#
 
 #%% manual iteration:
 #=
